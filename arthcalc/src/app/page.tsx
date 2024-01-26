d0a21c895cae581d572efc2a4b3595a2fdedc10a
Kevin <kevin.hanna@profitisle.com>
2024-01-26 23:02:12 +0200

    1

---
+++ arthcalc/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24">
+      <p className="text-5xl font-bold capitalize">wassap beyjin</p>
+      <p className="text-2xl font-thin capitalize">wassap beyjin</p>
+    </main>
+  );
+}

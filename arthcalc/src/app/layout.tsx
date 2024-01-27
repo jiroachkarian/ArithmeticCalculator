f81be0a31dd0313b44bd32f97a65bda495230282
jiroachkarian <jiroachkarian@gmail.com>
2024-01-27 12:48:19 +0200

    5

--- arthcalc/src/app/layout.tsx
+++ arthcalc/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <p>JACK SPARROW</p>
+      <p> captain JACK SPARROW</p>
     </html>
   );
 }
